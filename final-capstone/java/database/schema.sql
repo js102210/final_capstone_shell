ec0a3e3ffb86f20b01ff22ebcb64d2f974c48667
Keith Murray <keithanthonymurray@yahoo.com>
2021-04-05 12:03:29 -0400

    DB initial commit

--- final-capstone/java/database/schema.sql
+++ final-capstone/java/database/schema.sql
@@ -10,6 +10,49 @@ CREATE SEQUENCE seq_user_id
   CACHE 1;
 
 
+  
+CREATE SEQUENCE seq_size_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+  
+CREATE SEQUENCE seq_flavor_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+CREATE SEQUENCE seq_frosting_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+CREATE SEQUENCE seq_filling_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+CREATE SEQUENCE seq_extra_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+CREATE SEQUENCE seq_message_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;   
+     
+CREATE SEQUENCE seq_cake_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
 CREATE TABLE users (
 	user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
 	username varchar(50) NOT NULL,
@@ -21,5 +64,30 @@ CREATE TABLE users (
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
-
+DROP TABLE IF EXISTS styles;
+CREATE SEQUENCE seq_style_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+CREATE TABLE styles
+(
+        style_id int DEFAULT nextval('seq_style_id'::regclass) NOT NULL,
+        style_name VARCHAR NOT NULL,
+        is_available BOOLEAN NOT NULL DEFAULT TRUE,
+        price_mod DECIMAL (19,2) NOT NULL DEFAULT 0.00,
+        
+        CONSTRAINT pk_styles PRIMARY KEY (style_id)
+        ); 
+        
+        
+        
+CREATE TABLE 
+(
+        cake_id int DEFAULT nextval('seq_cake_id'::regclass) NOT NULL,        
+CREATE TABLE cakes
+(
+        cake_id int DEFAULT nextval('seq_cake_id'::regclass) NOT NULL,
+        
+  
 COMMIT TRANSACTION;
