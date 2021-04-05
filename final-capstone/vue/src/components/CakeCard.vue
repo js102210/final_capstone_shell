cfe68a48c038ead448a271d7b57a665e452ca98f
Jonathan Guillen <jonathanguillencontact@gmail.com>
2021-04-05 13:23:01 -0400

    update vue

---
+++ final-capstone/vue/src/components/CakeCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h3>{{cake.name}}</h3>
+      <img src="{{cake.image_url}}" />
+
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: "cake-card",
+    props: [
+        "cake"
+    ]
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ final-capstone/vue/src/components/CakeList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <cake-card />
+  </div>
+</template>
+
+<script>
+import CakeCard from './CakeCard.vue'
+export default {
+  components: { CakeCard },
+
+}
+</script>
+
+<style>
+
+</style>
