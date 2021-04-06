5773feb0d57c6460234011dfd9ac89509ebc8984
Keith Murray <keithanthonymurray@yahoo.com>
2021-04-06 12:16:29 -0400

    dummydata migrated to schema.sql

--- final-capstone/java/database/dummydata.sql
+++ final-capstone/java/database/dummydata.sql
@@ -33,8 +33,10 @@ INSERT INTO statuses (status_name)
 	VALUES ('Complete');
 
 INSERT INTO orders (status_id, total_price, datetime_placed, datetime_delivery, customer_name, customer_phone_number)
-	VALUES ('Pending', 14.99, '2021-4-6 11:18:55', '2021-4-7 12:00:00', 'Ernest Hemingway', 5138675309);
+	VALUES (1, 14.99, '2021-4-6 11:18:55', '2021-4-7 12:00:00', 'Ernest Hemingway', 5138675309);
 
-INSERT INTO cake_config (cake_config_name, cake_config_img_url, cake_config_description)
-	VALUES('
+INSERT INTO cake_config (cake_config_name, cake_config_img_url, cake_config_description, flavor_id, frosting_id, filling_id)
+	VALUES('Confetti Birthday Cake', 'https://preppykitchen.com/wp-content/uploads/2018/04/Funfetti-original-redone-blog-1.jpg', 
+	'Our fun-filled confetti cake with luscious vanilla frosting', 4, 2, 1
+	);
 
