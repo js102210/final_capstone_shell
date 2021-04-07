7c5d2d27009e0f1d2361f9da7e741e71f51b99fa
Keith Murray <keithanthonymurray@yahoo.com>
2021-04-07 10:24:33 -0400

    initial OrderController made, CustomCakeConfig to follow

---
+++ final-capstone/java/src/main/java/com/techelevator/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.OrderDAO;
+import com.techelevator.model.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class OrderController {
+    private OrderDAO orderDAO;
+
+    public OrderController(OrderDAO orderDAO) {
+        this.orderDAO = orderDAO;
+    }
+
+    /**
+     *
+     */
+    @RequestMapping( path= "/placeorder", method = RequestMethod.POST)
+    @ResponseStatus (value= HttpStatus.CREATED)
+    public void addOrder(@RequestBody Order order) {
+        orderDAO.placeOrder (order);
+    }
+}
