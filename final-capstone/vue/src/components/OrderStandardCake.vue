bf601afab03a94ad129849416d9ba4cdfed98ccd
Dan Mess <d.roman.mess@gmail.com>
2021-04-06 20:24:13 -0400

    implemented all drop down selects for standard cake order module. next is constructing the order cake JSON and then the method to submit the form.

--- final-capstone/vue/src/components/OrderStandardCake.vue
+++ final-capstone/vue/src/components/OrderStandardCake.vue
@@ -10,15 +10,33 @@
       <label for="standard cake selection">Select your standard cake:</label>
       <!-- the v-model sets the select to whatever the standardCakeIdForOrder is. If null, it's blank.-->
       <select name="standard cake" v-model="selected">
-          <!-- sets text for options to all cake names in placeholderCakes. value is the cake's cake_id-->
+          <!-- sets text for options to all cake names in standardCakeConfigs. value is the cake's cake_id-->
           <option 
-          v-for="cake in $store.state.placeholderCakes" 
-          v-bind:key="cake.cake_id"
-          v-bind:value="{id: cake.cake_id, name: cake.name, description: cake.description, image: cake.image_url}"
+          v-for="cake in $store.state.standardCakeConfigs" 
+          v-bind:key="cake.cake_config_id"
+          v-bind:value="{id: cake.cake_config_id, name: cake.cake_config_name, description: cake.cake_config_description, image: cake.cake_config_img_url}"
           >
-              {{cake.name}}
+              {{cake.cake_config_name}}
               </option>
           </select><br>
+          <label for="standard cake size">Select your size:</label>
+          <select name="standard cake size">
+          <option
+          v-for="size in $store.state.availableCakeSizes"
+          v-bind:key="size.size_id"
+          v-bind:value="{id: size.size_id, name: size.size_name}">
+          {{size.size_description}}
+          </option>
+          </select> <br>
+          <label for="standard cake style">Select your cake style:</label>
+            <select name="standard cake style">
+          <option
+          v-for="style in $store.state.availableCakeStyles"
+          v-bind:key="style.style_id"
+          v-bind:value="{id: style.style_id, name: style.style_name}">
+          {{style.style_name}}
+          </option>
+          </select> <br>
       <label for="optional message">Put what message you want on your cake! (Optional: $1.50 extra)</label>
       <input name="message" type="text"><br>
       <label for="pickup time">When do you want to pick up your cake?</label>
@@ -56,6 +74,7 @@ export default {
 .order-display {
     display: flex;
     flex-wrap: wrap;
+    flex-direction: column;
     align-items: center;
 }
 
@@ -64,7 +83,7 @@ export default {
     justify-content: space-evenly;
     flex-wrap: wrap;
     align-items: center;
-    width: 150px;
+    width: 400px;
     border: 2px solid black;
     border-radius: 10px;
     padding: 20px;
