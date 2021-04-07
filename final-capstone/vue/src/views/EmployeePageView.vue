9983d2998056b316848101fff215e32146d2240d
Jonathan Guillen <jonathanguillencontact@gmail.com>
2021-04-07 18:56:39 -0400

    adding employee page framework

---
+++ final-capstone/vue/src/views/EmployeePageView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <emp-create-cake></emp-create-cake>
+      <emp-change-cake-availabilty></emp-change-cake-availabilty>
+      <emp-create-options></emp-create-options>
+      <emp-change-option-availabilty></emp-change-option-availabilty>
+  </div>
+</template>
+
+<script>
+import EmpChangeCakeAvailabilty from '../components/EmpChangeCakeAvailabilty.vue'
+import EmpChangeOptionAvailabilty from '../components/EmpChangeOptionAvailabilty.vue'
+import EmpCreateCake from '../components/EmpCreateCake.vue'
+import EmpCreateOptions from '../components/EmpCreateOptions.vue'
+export default {
+    name: employee-page-view,
+  components: { 
+      EmpCreateCake, 
+      EmpChangeCakeAvailabilty, 
+      EmpCreateOptions, 
+      EmpChangeOptionAvailabilty },
+}
+</script>
+
+<style>
+
+</style>
