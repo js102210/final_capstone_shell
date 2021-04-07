345ab742e1c3775b1e8b43b5970cc22ad84fd2f0
Keith Murray <keithanthonymurray@yahoo.com>
2021-04-07 12:16:53 -0400

    merge conflict fun

--- final-capstone/java/src/main/java/com/techelevator/controller/OrderController.java
+++ final-capstone/java/src/main/java/com/techelevator/controller/OrderController.java
@@ -4,7 +4,7 @@ import com.techelevator.dao.OrderDAO;
 import com.techelevator.model.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-<<<<<<< HEAD
+
 
 @RestController
 public class OrderController {
@@ -18,7 +18,7 @@ public class OrderController {
      *
      */
     @RequestMapping( path= "/placeorder", method = RequestMethod.POST)
-=======
+
 @RestController
 public class OrderController {
     private OrderDAO orderDAO;
@@ -29,7 +29,7 @@ public class OrderController {
      *
      */
     @RequestMapping( path= "/placeOrder", method = RequestMethod.POST)
->>>>>>> 96ea632251e2be4fbdb3acdf8e5c4d7827c8eb95
+
     @ResponseStatus (value= HttpStatus.CREATED)
     public void addOrder(@RequestBody Order order) {
         orderDAO.placeOrder (order);
