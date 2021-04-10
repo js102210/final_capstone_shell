9caa48fea9d34d980f374d361911b2015cd58f5f
Dan Mess <d.roman.mess@gmail.com>
2021-04-10 15:36:00 -0400

    I believe I've written all of the front end API calls. I'll be back by 3:50 pm and I'll review to make sure I've covered everything.

--- final-capstone/vue/src/services/EmployeeService.js
+++ final-capstone/vue/src/services/EmployeeService.js
@@ -1,13 +1,94 @@
 import axios from 'axios';
 
 export default {
+//note - all API calls in Employee Service require authorization
 
-    // getStandardConfigs(){
-    //     return axios.get('/configs');
-    // },
+getAllConfigs(){
+    return axios.get('/configs');
+},
 
-    // sendOrderJSON(orderJSON){
-    //     return axios.post('/placeOrder', orderJSON);
-    // }
+getAllFlavors(){
+    return axios.get('/flavors');
+},
+
+getAllFrostings(){
+    return axios.get('/frostings');
+},
+
+getAllFillings(){
+    return axios.get('/fillings');
+},
+
+getAllStyles(){
+    return axios.get('/fillings');
+},
+
+getAllSizes(){
+    return axios.get('/sizes');
+},
+
+getAllExtras(){
+    return axios.get('/extras');
+},
+
+createCakeConfig(ConfigJSON){
+    return axios.post('/configs', ConfigJSON);
+},
+
+createFlavor(FlavorJSON){
+    return axios.post('/flavors', FlavorJSON);
+},
+
+createFrosting(FrostingJSON){
+    return axios.post('/frostings', FrostingJSON);
+},
+
+createFilling(FillingJSON){
+    return axios.post('/fillings', FillingJSON);
+},
+
+createStyle(StyleJSON){
+    return axios.post('/styles', StyleJSON);
+},
+
+createSize(SizeJSON){
+    return axios.post('/sizes', SizeJSON);
+},
+
+createExtra(ExtraJSON){
+    return axios.post('extras', ExtraJSON);
+},
+
+updateCakeConfig(ConfigJSON){
+    return axios.post('/configs', ConfigJSON);
+},
+
+updateFlavor(FlavorJSON){
+    return axios.post('/flavors', FlavorJSON);
+},
+
+updateFrosting(FrostingJSON){
+    return axios.post('/frostings', FrostingJSON);
+},
+
+updateFilling(FillingJSON){
+    return axios.post('/fillings', FillingJSON);
+},
+
+updateStyle(StyleJSON){
+    return axios.post('/styles', StyleJSON);
+},
+
+updateSize(SizeJSON){
+    return axios.post('/sizes', SizeJSON);
+},
+
+updateExtra(ExtraJSON){
+    return axios.post('extras', ExtraJSON);
+},
+
+updateOrder(orderJSON){
+    return axios.put(`orders/${orderJSON.orderID}`, orderJSON);
+},
 
 }
