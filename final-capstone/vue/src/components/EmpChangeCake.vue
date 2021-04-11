0b3f462ff851e0b452a0bcc39c683029e347c8a1
Jonathan Guillen <jonathanguillencontact@gmail.com>
2021-04-10 20:01:57 -0400

    updating Create Options, ready for plug and play, taking dinner break

--- final-capstone/vue/src/components/EmpChangeCake.vue
+++ final-capstone/vue/src/components/EmpChangeCake.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="change-cake-display">
     <p>Edit Standard Cakes:</p>
+
+    <table id="standard-cakes">
+      <thead>
+        <tr>
+          <th>&nbsp;</th>
+          <th>Cake Name</th>
+          <th>Flavor</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+<!--       
+      <tbody>
+        <tr>
+          <td>
+            <select id="statusFilter" v-model="filter.status">
+              <option value>Show All</option>
+              <option value="Active">Active</option>
+              <option value="Disabled">Disabled</option>
+            </select>
+          </td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr v-for="user in filteredList" v-bind:key="user.id"
+          v-bind:class="{ disabled: user.status === 'Disabled' }">
+          <td>
+            <input type="checkbox" v-bind:id="user.id" v-bind:value="user.id" v-model="selectedUserIDs" />
+          </td>
+          <td>{{ user.firstName }}</td>
+          <td>{{ user.lastName }}</td>
+          <td>{{ user.username }}</td>
+          <td>{{ user.emailAddress }}</td>
+          <td>{{ user.status }}</td>
+          <td>
+            <button class="btnEnableDisable" v-on:click="flipStatus(user.id)" >
+              {{ user.status === "Active" ? "Disable" : "Enable" }}</button>
+          </td> 
+        </tr>
+      </tbody> -->
+    </table>
+
   </div>
 </template>
 
--- final-capstone/vue/src/components/EmpCreateOptions.vue
+++ final-capstone/vue/src/components/EmpCreateOptions.vue
@@ -1,19 +1,63 @@
 <template>
   <div class="create-options-display">
     <p>Create New Options:</p>
+    <p>Required Fields (*) must be completed</p>
 
-      <label for="cakeFlavor">Cake Flavor:</label>
-      <input type="text" name="cakeFlavor" v-model="newCakeFlavor" />
-      <button>Create New Flavor</button>
+    <form @submit="createNewFlavor">
+      <label for="Flavor">Flavor: *</label>
+      <input type="text" name="Flavor" v-model="newFlavor.flavorName" />
+      <label for="Flavor price mod">Price Modifier: $</label>
+      <input type="number" name="Flavor Price mod" step="0.01" placeholder="0.00"
+        v-model="newFlavor.priceMod" min="0"/>
+      <button type="submit" :disabled="!newFlavor.flavorName">Create New Flavor</button>
+    </form><br />
 
-      <label for="cakeFrosting">Cake Frosting:</label>
-      <input type="text" name="cakeFrosting" v-model="newCakeFrosting" />
-      <button>Create New Frosting</button>
-      
-      <label for="cakeFilling">Cake Filling:</label>
-      <input type="text" name="cakeFilling" v-model="newCakeFilling" />
-      <button>Create New Filling</button>
+    <form @submit="createNewFrosting">
+      <label for="Frosting">Frosting: *</label>
+      <input type="text" name="Frosting" v-model="newFrosting.frostingName" />
+      <label for="Frosting price mod">Price Modifier: $</label>
+      <input type="number" name="Frosting Price mod" step="0.01" placeholder="0.00"
+        v-model="newFrosting.priceMod" min="0"/>
+      <button type="submit" :disabled="!newFrosting.frostingName">Create New Frosting</button>
+    </form><br />
 
+    <form @submit="createNewFilling">
+      <label for="Filling">Filling: *</label>
+      <input type="text" name="Filling" v-model="newFilling.fillingName" />
+       <label for="Filling price mod">Price Modifier: $</label>
+      <input type="number" name="Filling Price mod" step="0.01" placeholder="0.00"
+        v-model="newFilling.priceMod" min="0"/>
+      <button type="submit" :disabled="!newFilling.fillingName">Create New Filling</button>
+    </form><br />
+
+    <form @submit="createNewExtra">
+      <label for="Extra">Extra: *</label>
+      <input type="text" name="Extra" v-model="newExtra.extraName" />
+       <label for="Extra price mod">Price Modifier: $</label>
+      <input type="number" name="Extra Price mod" step="0.01" placeholder="0.00"
+        v-model="newExtra.priceMod" min="0"/>
+      <button type="submit" :disabled="!newExtra.extraName">Create New Extra</button>
+    </form><br />
+
+     <form @submit="createNewStyle">
+      <label for="Style">Style: *</label>
+      <input type="text" name="Style" v-model="newStyle.styleName" />
+       <label for="Style price mod">Price Modifier: $</label>
+      <input type="number" name="Style Price mod" step="0.01" placeholder="0.00"
+        v-model="newStyle.priceMod" min="0"/>
+      <button type="submit" :disabled="!newStyle.styleName">Create New Style</button>
+    </form><br />
+
+     <form @submit="createNewSize">
+      <label for="Size">Size: *</label>
+      <input type="text" name="Size" v-model="newSize.sizeName" />
+       <label for="Size description">Description: *</label>
+      <input type="text" name="Size description" v-model="newSize.sizeDescription" />
+       <label for="Size price mod">Price Modifier: $</label>
+      <input type="number" name="Size Price mod" step="0.01" placeholder="0.00"
+        v-model="newSize.priceMod" min="0"/>
+      <button type="submit" :disabled="!createSizeFormValidated">Create New Size</button>
+    </form><br />
 
 </div>
 </template>
@@ -22,15 +66,67 @@
 export default {
   data() {
     return {
-      newCakeFlavor: "",
-      newCakeFrosting: "",
-      newCakeFilling: "",
+      newFlavor: {
+        flavorName: "",
+        isAvailable: true,
+        priceMod: 0.0,
+      },
+      newFrosting: {
+        frostingName: "",
+        isAvailable: true,
+        priceMod: 0.0,
+      },
+      newFilling: {
+        fillingName: "",
+        isAvailable: true,
+        priceMod: 0.0,
+      },
+      newExtra  : {
+        extraName: "",
+        isAvailable: true,
+        priceMod: 0.0,
+      },
+      newStyle: {
+        styleName: "",
+        isAvailable: true,
+        priceMod: 0.0,
+      },
+      newSize: {
+        sizeName: "",
+        sizeDescription: "",
+        isAvailable: true,
+        priceMod: 0.0,
+      }
+    }
+  },
+  computed:{
+    createSizeFormValidated() {
+       if (this.newSize.sizeName != "" && this.newSize.sizeDescription !=""){
+        return true;
+      } else {
+        return false;
+    }
     }
   },
   methods: {
-  createFlavor(){
-    // call add flavor mutation
-  }
+  createNewFlavor(){
+
+  },
+   createNewFrosting(){
+
+  },
+   createNewFilling(){
+
+  },
+   createNewExtra(){
+
+  },
+   createNewStyle(){
+
+  },
+   createNewSize(){
+
+  },
   }
 }
 </script>
