abbefeb164ff779c18282fc0ef48b74ac5a77879
Dan Mess <d.roman.mess@gmail.com>
2021-04-12 10:22:11 -0400

    did documentation for cake config controller methods

--- final-capstone/java/src/main/java/com/techelevator/controller/CakeConfigController.java
+++ final-capstone/java/src/main/java/com/techelevator/controller/CakeConfigController.java
@@ -18,20 +18,30 @@ public class CakeConfigController {
         this.cakeConfigDAO = cakeConfigDAO;
     }
 
-    //get all configs - this needs to require authorization. employee only.
+    /**
+     * Gets the list of *all* cake configs, whether or not available. Employee only.
+     * @return - returns the list of ALL cake configs regardless of availability.
+     */
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(path = "/configs", method = RequestMethod.GET)
     public List <CakeConfig> getAllConfigsFromDb() {
         return cakeConfigDAO.getAllConfigs ();
     }
 
-    //gets *available* configs for customers
+    /**
+     * Gets the list of available cake configs. Used for customer portion of website.
+     * @return returns a list of available cake configs.
+     */
     @RequestMapping(path = "/configs/available", method = RequestMethod.GET)
     public List <CakeConfig> getAvailableConfigsFromDb() {
         return cakeConfigDAO.getAvailableConfigs ();
     }
 
-    //this also requires authorization. employee only.
+    /**
+     * Creates a new cake config. Employee only.
+     * @param cakeConfig - the Cake Config object passed in from front end.
+     * @return - returns the id of the new cake config.
+     */
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(path = "/configs", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
@@ -39,7 +49,11 @@ public class CakeConfigController {
         return cakeConfigDAO.addCakeConfig (cakeConfig);
     }
 
-    //this also requires authorization. employee only.
+    /**
+     * flips a cake config's availability. Employee only.
+     * @param id - the id of the cake config to be flipped
+     * @return - returns the new flipped boolean value of isAvailable.
+     */
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(path = "/configs/flip/{id}", method = RequestMethod.PATCH)
     @ResponseStatus(value = HttpStatus.ACCEPTED)
