c6a03d404f08bcca54fc402a7adad820d1a46200
Dan Mess <d.roman.mess@gmail.com>
2021-04-12 13:08:34 -0400

    status contoller / API fully implemented and tested in Postman

--- final-capstone/java/src/main/java/com/techelevator/controller/StatusController.java
+++ final-capstone/java/src/main/java/com/techelevator/controller/StatusController.java
@@ -1,9 +1,28 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.StatusDAO;
+
+import com.techelevator.model.Status;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 public class StatusController {
+
+    private final StatusDAO statusDAO;
+    public StatusController(StatusDAO statusDAO){
+        this.statusDAO = statusDAO;
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(path = "/statuses", method = RequestMethod.GET)
+    public List<Status> getStatuses(){
+        return statusDAO.getStatuses();
+    }
 }
