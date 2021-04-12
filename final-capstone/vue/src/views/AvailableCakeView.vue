578cd8182654c8fd9e6c551aaa295b31051d6485
Jonathan Guillen <jonathanguillencontact@gmail.com>
2021-04-12 17:12:07 -0400

    pulled style out of cake card component and placed into availablecakeview

--- final-capstone/vue/src/views/AvailableCakeView.vue
+++ final-capstone/vue/src/views/AvailableCakeView.vue
@@ -13,5 +13,31 @@ export default {
 </script>
 
 <style>
+.cake-container {
+    display:flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    align-items: center;
+    width: 160px;
+    height: 500px;
+    border: 2px solid black;
+    border-radius: 10px;
+    padding: 20px;
+    margin-top: 20px;
+    background: rgba(255, 255, 255, 0.349);
+    /* -webkit-backdrop-filter: blur(20px);
+	backdrop-filter: blur(10px); */
+}
+
+.cake-pic {
+    width: 100%;
+    height: 30%;
+}
+
+.cake-container p {
+    font-family: 'Quicksand';
+    font-size: .9rem;
+}
+
 
 </style>
