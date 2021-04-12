2b56e7dacd74ce738d5ff7d664e5c546d4342679
Jonathan Guillen <jonathanguillencontact@gmail.com>
2021-04-11 22:43:45 -0400

    update EmpChangeOptions

--- final-capstone/vue/src/components/EmpChangeOptions.vue
+++ final-capstone/vue/src/components/EmpChangeOptions.vue
@@ -2,30 +2,136 @@
 <div class="change-options-display">
     <p>Edit Cake Options:</p>
 
-       <table id="standard-cakes">
+         <table id="flavors">
       <thead>
         <tr>
-          <th>&nbsp;</th>
+          <th>Flavors:</th>
           <th>Name</th>
-          <th>Description</th>
           <th>Status</th>
           <th>Action</th>
         </tr>
       </thead>
       
       <tbody>
-        <tr v-for="cake in $store.state.availableCakeConfigsBE" v-bind:key="cake.cakeConfigID"
-            v-bind:class="{ unavailable: !cake.isAvailable}" >
+        <tr v-for="flavor in $store.state.allFlavorsBE" v-bind:key="flavor.flavorID"
+            v-bind:class="{ unavailable: !flavor.isAvailable}" >
           <td>
-            <input type="radio" name="flip status" v-bind:id="cake.cakeConfigID"
-              v-bind:value="cake.cakeConfigID" v-model="selectedCakeID" />
+            <input type="radio" name="flip status" v-bind:id="flavor.flavorID"
+              v-bind:value="flavor.flavorID" v-model="selectedFlavorID" />
           </td>
-          <td>{{cake.cakeConfigName}}</td>
-          <td>{{cake.cakeConfigDescription}}</td>
-          <td>{{cake.isAvailable? "Available": "Unavailable"}}</td>
+          <td>{{flavor.flavorName}}</td>
+          <td>{{flavor.isAvailable? "Available": "Unavailable"}}</td>
           <td>
-            <button class="btnAvailableUnavailable" v-on:click="flipStatus(cake.cakeConfigID)">
-              {{ cake.isAvailable ? "Make Unavailable" : "Make Available" }} </button>
+            <button class="btnAvailableUnavailable" v-on:click="flipStatus(flavor.flavorID)">
+              {{ flavor.isAvailable ? "Make Unavailable" : "Make Available" }} </button>
+          </td> 
+        </tr>
+      </tbody>
+    </table>
+
+      <table id="frostings">
+      <thead>
+        <tr>
+          <th>Frostings:</th>
+          <th>Name</th>
+          <th>Status</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      
+      <tbody>
+        <tr v-for="frosting in $store.state.allFrostingsBE" v-bind:key="frosting.frostingID"
+            v-bind:class="{ unavailable: !frosting.isAvailable}" >
+          <td>
+            <input type="radio" name="flip status" v-bind:id="frosting.frostingID"
+              v-bind:value="frosting.frostingID" v-model="selectedFrostingID" />
+          </td>
+          <td>{{frosting.frostingName}}</td>
+          <td>{{frosting.isAvailable? "Available": "Unavailable"}}</td>
+          <td>
+            <button class="btnAvailableUnavailable" v-on:click="flipStatus(frosting.frostingID)">
+              {{ frosting.isAvailable ? "Make Unavailable" : "Make Available" }} </button>
+          </td> 
+        </tr>
+      </tbody>
+    </table>
+
+      <table id="fillings">
+      <thead>
+        <tr>
+          <th>Fillings:</th>
+          <th>Name</th>
+          <th>Status</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      
+      <tbody>
+        <tr v-for="filling in $store.state.allFillingsBE" v-bind:key="filling.fillingID"
+            v-bind:class="{ unavailable: !filling.isAvailable}" >
+          <td>
+            <input type="radio" name="flip status" v-bind:id="filling.fillingID"
+              v-bind:value="filling.fillingID" v-model="selectedFillingID" />
+          </td>
+          <td>{{filling.fillingName}}</td>
+          <td>{{filling.isAvailable? "Available": "Unavailable"}}</td>
+          <td>
+            <button class="btnAvailableUnavailable" v-on:click="flipStatus(filling.fillingID)">
+              {{ filling.isAvailable ? "Make Unavailable" : "Make Available" }} </button>
+          </td> 
+        </tr>
+      </tbody>
+    </table>
+
+          <table id="extras">
+      <thead>
+        <tr>
+          <th>Extras:</th>
+          <th>Name</th>
+          <th>Status</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      
+      <tbody>
+        <tr v-for="extra in $store.state.allExtrasBE" v-bind:key="extra.extraID"
+            v-bind:class="{ unavailable: !extra.isAvailable}" >
+          <td>
+            <input type="radio" name="flip status" v-bind:id="extra.extraID"
+              v-bind:value="extra.extraID" v-model="selectedExtraID" />
+          </td>
+          <td>{{extra.extraName}}</td>
+          <td>{{extra.isAvailable? "Available": "Unavailable"}}</td>
+          <td>
+            <button class="btnAvailableUnavailable" v-on:click="flipStatus(extra.extraID)">
+              {{ extra.isAvailable ? "Make Unavailable" : "Make Available" }} </button>
+          </td> 
+        </tr>
+      </tbody>
+    </table>
+
+      <table id="flavors">
+      <thead>
+        <tr>
+          <th>Styles:</th>
+          <th>Name</th>
+          <th>Status</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      
+      <tbody>
+        <tr v-for="style in $store.state.allCakeStylesBE" v-bind:key="style.styleID"
+            v-bind:class="{ unavailable: !style.isAvailable}" >
+          <td>
+            <input type="radio" name="flip status" v-bind:id="style.styleID"
+              v-bind:value="style.styleID" v-model="selectedStyleID" />
+          </td>
+          <td>{{style.styleName}}</td>
+          <td>{{style.isAvailable? "Available": "Unavailable"}}</td>
+          <td>
+            <button class="btnAvailableUnavailable" v-on:click="flipStatus(style.styleID)">
+              {{ style.isAvailable ? "Make Unavailable" : "Make Available" }} </button>
           </td> 
         </tr>
       </tbody>
@@ -34,7 +140,7 @@
       <table id="sizes">
       <thead>
         <tr>
-          <th>Sizes</th>
+          <th>Sizes:</th>
           <th>Name</th>
           <th>Description</th>
           <th>Status</th>
@@ -72,8 +178,12 @@ export default {
 
   data() {
     return {
-      selectedCakeID: {},
-      selectedSizeID: {}
+      selectedSizeID: {},
+      selectedFlavorID: {},
+      selectedStylesID: {},
+      selectedFrostingID: {},
+      selectedFillingID: {},
+      selectedExtraID: {}
     }
   },
 
