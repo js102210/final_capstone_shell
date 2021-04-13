6e9589f14b853454ee88113a871ede2994684a51
Jake <jakeschwartz38@gmail.com>
2021-04-13 18:23:53 -0400

    added Order information other than items to OrderDetails component

--- final-capstone/vue/src/components/OrderDetails.vue
+++ final-capstone/vue/src/components/OrderDetails.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+      <h1>{{this.$store.state.selectedOrder.customerName}}</h1>
+      <h2>Ordered on {{this.$store.state.selectedOrder.orderDatePlaced}}</h2>
+      <h2>To be delivered on {{this.$store.state.selectedOrder.orderPickupDate}}
+            at {{this.$store.state.selectedOrder.orderPickupTime}}
+      </h2>
+      <h2>Items ordered:  <br></h2>
 
   </div>
 </template>
