c444a286b3caeb9c8dbe8bdae8be0af759437e57
Jake <jakeschwartz38@gmail.com>
2021-04-13 19:07:16 -0400

    added some store mutations to the OrderDetailsView in the created lifecycle hook to populate the ALL_AVAILABLE arrays with data on the various frostings, fillings, etc which we can pull from to display the names of items based on their IDs in the OrderDetails component

--- final-capstone/vue/src/views/OrderDetailsView.vue
+++ final-capstone/vue/src/views/OrderDetailsView.vue
@@ -6,9 +6,24 @@
 
 <script>
 import OrderDetails from '../components/OrderDetails.vue'
+import EmployeeService from '../services/EmployeeService'
 
 export default {
   components: { OrderDetails },
+  created(){
+    EmployeeService.getAllFrostings().then((response)=> {
+      this.$store.commit("SET_ALL_FROSTINGS_ARRAY", response.data);
+    });
+    EmployeeService.getAllFillings().then((response)=>{
+      this.$store.commit("SET_ALL_FILLINGS_ARRAY", response.data)
+    });
+    EmployeeService.getAllStyles().then((response)=>{
+      this.$store.commit("SET_ALL_STYLES_ARRAY", response.data)
+    });
+    EmployeeService.getAllSizes().then((response) => {
+      this.$store.commit("SET_ALL_SIZES_ARRAY", response.data)
+    });
+  }
 
 }
 </script>
