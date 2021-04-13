6d3be3bca6873e1f2872aa8c184af6b21ad526a8
Jake <jakeschwartz38@gmail.com>
2021-04-13 17:13:59 -0400

    added orderDetails component and imported it in the OrderDetailsView

--- final-capstone/vue/src/views/OrderDetailsView.vue
+++ final-capstone/vue/src/views/OrderDetailsView.vue
@@ -1,9 +1,14 @@
 <template>
-  
+  <div class="order-details">
+<order-details/>
+  </div>
 </template>
 
 <script>
+import OrderDetails from '../components/OrderDetails.vue'
+
 export default {
+  components: { OrderDetails },
 
 }
 </script>
