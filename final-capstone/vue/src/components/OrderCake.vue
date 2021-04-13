f8c9d5164060a581a7f38aeeeb5577c118f8868e
Jonathan Guillen <jonathanguillencontact@gmail.com>
2021-04-12 20:51:29 -0400

    update unsaved

--- final-capstone/vue/src/components/OrderCake.vue
+++ final-capstone/vue/src/components/OrderCake.vue
@@ -391,55 +391,5 @@ export default {
 </script>
 
 <style>
-.order-display {
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  align-items: center;
-}
 
-.order-form {
-  background: rgba(255, 255, 255, 0.349);
-  -webkit-backdrop-filter: blur(20px);
-  backdrop-filter: blur(10px);
-  padding: 17px;
-  border-radius: 10px;
-  border: 1px solid rgba(0, 0, 0, 0.61);
-}
-
-.cakedisplay {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  align-items: center;
-  width: 300px;
-  height: auto;
-  border: 1px solid rgba(0, 0, 0, 0.356);
-  border-radius: 10px;
-  padding: 20px;
-  margin-top: 20px;
-  background: rgba(255, 255, 255, 0.5);
-  /* -webkit-backdrop-filter: blur(10px);
-	backdrop-filter: blur(10px); */
-}
-
-.order-form input,
-select {
-  margin: 6px;
-}
-
-.cakedisplay img {
-  width: 70%;
-  height: 120px;
-}
-
-.cakedisplay h3 {
-  margin-top: -3%;
-}
-
-#calculated-price {
-  color: rgb(182, 8, 43);
-  font-weight: 700;
-  font-size: 1.6rem;
-}
 </style>
