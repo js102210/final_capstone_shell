89c19244540c5bdf6ebc26663929402eb0ecd4e3
Jake <jakeschwartz38@gmail.com>
2021-04-13 23:03:25 -0400

    added mutation calls ItemDetails component depends on to orderListView instead of OrderDetailsView. Note- created lifecycle hooks cannot be used to render things that the template items depend on. Thought that was the entire point but okay.

---
+++ final-capstone/vue/src/components/cakeItemDetails.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <h2>{{cakeItemJSON.cakeItemID}}: {{cakeConfigName}}</h2>
+
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            config: {},
+            cakeConfigName: '',
+            styleName: '',
+            sizeName: '',
+            message: '',
+            fillingName: '',
+            frostingName: '',
+            flavorName: '',
+            extras: []
+        }
+    },
+    props: [
+        'cakeItemJSON'
+    ],
+    created(){
+    this.config = this.$store.state.allCakeConfigsBE.find(
+    (config)=> config.cakeConfigID == this.cakeItemJSON.cakeItemConfigID)
+    this.cakeConfigName = this.config.cakeConfigName;   
+     
+    }
+
+    
+}
+</script>
+
+<style>
+
+</style>
