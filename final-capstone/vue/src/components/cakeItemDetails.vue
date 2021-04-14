028c38e60018891396f7a98eee6858a08f613284
Jake <jakeschwartz38@gmail.com>
2021-04-13 23:19:11 -0400

    ItemDetails component now populates with the style and size names of the item

--- final-capstone/vue/src/components/cakeItemDetails.vue
+++ final-capstone/vue/src/components/cakeItemDetails.vue
@@ -1,6 +1,8 @@
 <template>
 <div>
     <h2>{{cakeItemJSON.cakeItemID}}: {{cakeConfigName}}</h2>
+    <h3>Style: {{styleName}}   Size:  {{sizeName}}</h3>
+
 
 </div>
 </template>
@@ -10,6 +12,11 @@ export default {
     data(){
         return{
             config: {},
+            style: {},
+            size: {},
+            filling: {},
+            frosting: {},
+            flavor: {},
             cakeConfigName: '',
             styleName: '',
             sizeName: '',
@@ -26,9 +33,18 @@ export default {
     created(){
     this.config = this.$store.state.allCakeConfigsBE.find(
     (config)=> config.cakeConfigID == this.cakeItemJSON.cakeItemConfigID)
-    this.cakeConfigName = this.config.cakeConfigName;   
-     
+    this.cakeConfigName = this.config.cakeConfigName;  
+
+    this.style = this.$store.state.allCakeStylesBE.find(
+    (style) => style.styleID == this.cakeItemJSON.cakeItemStyleID)
+    this.styleName = this.style.styleName;
+
+    this.size = this.$store.state.allCakeSizesBE.find(
+    (size) => size.sizeID == this.cakeItemJSON.cakeItemSizeID)
+    this.sizeName = this.size.sizeName;
     }
+
+    
 
     
 }
